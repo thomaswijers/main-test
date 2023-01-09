272519c2f503f165fca18e9f32ff99dbcaf22e8b
LorangeJuicey <thomaswijers3@gmail.com>
2023-01-09 12:32:36 +0100

    firt commit

---
+++ components/header.php
@@ -0,0 +1,25 @@
+<?php
+echo '
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main test</title>
+    <link rel="stylesheet" href="Styles/css/reset.css">
+    <link rel="stylesheet" href="Styles/css/index.css">
+</head>';
+echo
+'<header>
+<div class="header-container">
+<img src="Styles/images/header-img.png" alt="header-img" class="header-img"> 
+<div class="menu-container">
+<ul>
+<li class="menu-item"><a href="index.php">Home</a></li>
+<li class="menu-item"><a href="index.php">Home</a></li>
+<li class="menu-item"><a href="index.php">Home</a></li>
+<li class="menu-item"><a href="index.php">Home</a></li>
+</ul>
+</div>
+</div>
+</header>';
+?>
